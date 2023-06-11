c9065e732ad40312303ff1a8ebba308a8dba7868
uthree <uthree271828@gmail.com>
2023-06-12 03:58:57 +0900

    Added ASR

--- dataset.py
+++ dataset.py
@@ -5,6 +5,22 @@ from tqdm import tqdm
 import os
 import random
 
+from janome.tokenizer import Tokenizer
+KATAKANA_LETTER_SMALL_A = 12449
+
+
+class PhoneticTokenizer:
+    def __init__(self):
+        self.janome_tokenizer = Tokenizer()
+
+    def tokenize(self, s):
+        phonetic = ""
+        tokens = self.janome_tokenizer.tokenize(s)
+        for token in tokens:
+            phonetic = phonetic + token.phonetic
+        phonetic = list(phonetic)
+        return [max(ord(p) - KATAKANA_LETTER_SMALL_A, 0) + 2 for p in phonetic]
+
 
 class WaveFileDirectory(torch.utils.data.Dataset):
     def __init__(self, source_dir_paths=[], length=65536, max_files=-1, sampling_rate=44100, shuffle_paths=True):
@@ -41,3 +57,55 @@ class WaveFileDirectory(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.data)
+
+
+
+class ASRDataset(torch.utils.data.Dataset):
+    def __init__(self, source_dir_path, max_tokens=30, max_len=262144, pad=0, eos=1, sampling_rate=44100):
+        super().__init__()
+        self.audio_path_list = []
+        self.text_path_list = []
+        self.waves = []
+        self.texts = []
+        tokenizer = PhoneticTokenizer()
+
+        formats = ["mp3", "wav", "ogg"]
+        print("Getting paths")
+        for fmt in formats:
+            self.audio_path_list += glob.glob(os.path.join(source_dir_path, f"**/*.{fmt}"), recursive=True)
+
+        for ap in self.audio_path_list:
+            noext = os.path.splitext(os.path.basename(ap))[0]
+            dirname = os.path.dirname(ap)
+            self.text_path_list.append(os.path.join(dirname, f"{noext}.txt"))
+
+        print("Loading")
+        for ap, tp in tqdm(zip(self.audio_path_list, self.text_path_list)):
+            wf, sr = torchaudio.load(ap)
+            # Resample
+            wf = torchaudio.functional.resample(wf, sr, sampling_rate)
+            # Crop
+            if wf.shape[1] > max_len:
+                wf = wf[:, :max_len]
+            else:
+                wf = torch.cat([wf, torch.zeros(1, max_len - wf.shape[1])], dim=1)
+
+            # Load text
+            with open(tp) as f:
+                p = tokenizer.tokenize(f.read())
+            p.append(eos)
+            while len(p) < max_tokens:
+                p.append(pad)
+
+            if len(p) > max_tokens:
+                p = p[:max_tokens]
+            p = torch.LongTensor(p)
+
+            self.waves.append(wf[0])
+            self.texts.append(p)
+
+    def __getitem__(self, index):
+        return self.waves[index], self.texts[index]
+
+    def __len__(self):
+        return len(self.waves)
